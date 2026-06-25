1c61e9fc66e35fe84073899744ed1eb2b25b1356
gagan947 <gagan.ctinfotech@gmail.com>
2026-06-25 16:00:23 +0530

    role page created

---
+++ src/app/component/choose-role/choose-role.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-role',
+  imports: [],
+  templateUrl: './choose-role.component.html',
+  styleUrl: './choose-role.component.css'
+})
+export class ChooseRoleComponent {
+
+}
